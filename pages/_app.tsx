e12cd89e7c58d1a3cd90036190f1a9371f2d52d2
dontdiewondering <90324800+dontdiewondering@users.noreply.github.com>
2022-04-15 08:50:07 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="shortcut icon" href="/khoomi-favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
       </Head>
       <Component {...pageProps} />
     </>
